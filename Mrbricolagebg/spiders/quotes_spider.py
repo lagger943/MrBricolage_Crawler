6f2c97b64a5f7d8d5961a995f248ea4a26d5e53c
Martin Mitov <lagger943@gmail.com>
2021-06-24 17:37:22 +0300

    Working store availability extraction

--- Mrbricolagebg/spiders/quotes_spider.py
+++ Mrbricolagebg/spiders/quotes_spider.py
@@ -20,52 +20,11 @@ class MrbricolageSpider(scrapy.Spider):
         return response.css(path).get().strip()
 
     def parse_product(self, response):
-        def spec_table_attributes_extract():
-
-            [self.product.update({'brand': key['value']}) for key in specs_table if "Марка" in key['key']]
-
-            [self.product.update({'model': key['value']}) for key in specs_table if "Модел" in key['key']]
-
-            [self.product.update({'origin': key['value']}) for key in specs_table if "Произход" in key['key']]
-
-            [self.product.update({'warranty': key['value']}) for key in specs_table if "Гаранция" in key['key']]
-
-        request_url = ""
-        self.product.update({'title': self.get_and_strip('h1.js-product-name::text', response)})
-
-        raw_price = self.get_and_strip('p.price.js-product-price::text', response)
-        if raw_price:
-            price = raw_price.replace(',', '.')
-            self.product.update({'price': price})
-
-        availability = self.get_and_strip('div.col-md-12.bricolage-availability::text', response)
-        if availability:
-            self.product.update({'availability': availability})
-
+        request_url={}
         article_text = self.get_and_strip('div.col-md-12.bricolage-code::text', response)
         if article_text:
             article_id = article_text.replace('Код Bricolage: ', '')
-            self.product.update({'article_id': article_id})
-
             request_url = f"https://mr-bricolage.bg/store-pickup/{article_id}/pointOfServices"
-
-        ean = response.css('div[id="home"] span::text').re('[^\s]+')[0]
-        if ean:
-            self.product.update({'ean': ean})
-
-        self.product.update({'url': response.url})
-
-        images = response.css('div.owl-thumb-item img::attr(src)').getall()
-        if images:
-            self.product.update({'images': images})
-
-        specs_table = [{'key': row.css('td:nth-child(1)::text').get().strip(),
-                        'value': row.css('td:nth-child(2)::text').get().strip()} for row
-                       in response.css('table.table tr')]
-
-        spec_table_attributes_extract()
-
-        self.product.update({"specs_table": specs_table})
 
         headers = {
             "Connection": "keep-alive",
@@ -96,19 +55,76 @@ class MrbricolageSpider(scrapy.Spider):
             dont_filter=True,
             cookies=cookies,
             headers=headers,
-            callback=self.parse_availability_info,
+            callback=self.parse_product_info,
+            meta={'page_response': response},
             body=body
         )
 
-    def parse_availability_info(self, response):
+    def parse_product_info(self, response):
+
+        def spec_table_attributes_extract():
+
+            [self.product.update({'brand': key['value']}) for key in specs_table if "Марка" in key['key']]
+
+            [self.product.update({'model': key['value']}) for key in specs_table if "Модел" in key['key']]
+
+            [self.product.update({'origin': key['value']}) for key in specs_table if "Произход" in key['key']]
+
+            [self.product.update({'warranty': key['value']}) for key in specs_table if "Гаранция" in key['key']]
+
+        page = response.meta.get('page_response')
+
+        self.product.update({'title': self.get_and_strip('h1.js-product-name::text', page)})
+
+        raw_price = self.get_and_strip('p.price.js-product-price::text', page)
+        if raw_price:
+            price = raw_price.replace(',', '.')
+            self.product.update({'price': price})
+
+        availability = self.get_and_strip('div.col-md-12.bricolage-availability::text', page)
+        if availability:
+            self.product.update({'availability': availability})
+
+        article_text = self.get_and_strip('div.col-md-12.bricolage-code::text', page)
+        if article_text:
+            article_id = article_text.replace('Код Bricolage: ', '')
+            self.product.update({'article_id': article_id})
+
+            request_url = f"https://mr-bricolage.bg/store-pickup/{article_id}/pointOfServices"
+
+        ean = page.css('div[id="home"] span::text').re('[^\s]+')[0]
+        if ean:
+            self.product.update({'ean': ean})
+
+        self.product.update({'url': page.url})
+
+        images = page.css('div.owl-thumb-item img::attr(src)').getall()
+        if images:
+            self.product.update({'images': images})
+
+        specs_table = [{'key': row.css('td:nth-child(1)::text').get().strip(),
+                        'value': row.css('td:nth-child(2)::text').get().strip()} for row
+                       in page.css('table.table tr')]
+
+        spec_table_attributes_extract()
+
+        self.product.update({"specs_table": specs_table})
+
         r = json.loads(response.text)
+
         store_availability = [{"store": key['displayName'], "Availability": key['stockPickup']} for key in r['data']]
-        self.product.update({"Availability": store_availability})
+
+        self.product.update({"Store Availability": store_availability})
+
         yield self.product
 
 
 class MrbricolageAvailabilitySpider(scrapy.Spider):
     name = "Availability"
+
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 1,
+    }
 
     product = {}
 
@@ -122,11 +138,13 @@ class MrbricolageAvailabilitySpider(scrapy.Spider):
 
 
     def parse_product(self, response):
+        request_url = ""
+
         article_text = self.get_and_strip('div.col-md-12.bricolage-code::text', response)
         if article_text:
             article_id = article_text.replace('Код Bricolage: ', '')
             request_url = f"https://mr-bricolage.bg/store-pickup/{article_id}/pointOfServices"
-            self.product.update({'URL': request_url})
+
         headers = {
             "Connection": "keep-alive",
             "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
@@ -145,25 +163,29 @@ class MrbricolageAvailabilitySpider(scrapy.Spider):
             "__utmz": "149670890.1624529510.9.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
             "JSESSIONID": response.headers.getlist('Set-Cookie')[0].decode("utf-8").split(";")[0].split("=")[1],
         }
+
         token = response.css('[name="CSRFToken"]::attr(value)').get()
 
         body = f'locationQuery=&cartPage=false&entryNumber=0&latitude=42.6641056&longitude=23.3233149&CSRFToken={token}'
 
-        yield Request(
+        return Request(
             url=request_url,
             method='POST',
             dont_filter=True,
             cookies=cookies,
             headers=headers,
             callback=self.parse_availability_info,
-            body=body
+            body=body,
+            meta={'page_response': response}
         )
 
     def get_and_strip(self, path, response):
         return response.css(path).get().strip()
 
     def parse_availability_info(self, response):
+        page = response.meta.get('page_response')
         r =json.loads(response.text)
+        self.product.update({'Request url': page.url})
         store_availability =[{"store": key['displayName'],"Availability": key['stockPickup']} for key in r['data']]
         self.product.update({"Availability": store_availability})
         yield self.product
